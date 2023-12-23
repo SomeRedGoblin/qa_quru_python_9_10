e120384602af5eb44d766f3034c3466073126eec
Andrey Rusak <dudzy@mail.ru>
2023-12-23 19:07:25 +0300

    Часть кода вынесена в модель

--- tests/test_qademo_user_registration.py
+++ tests/test_qademo_user_registration.py
@@ -1,43 +1,54 @@
 from selene import browser, have, be
 import os
+from model.pages.registration_page import RegistrationPage
 
 
 def test_complete_todo():
-    browser.open('/automation-practice-form')
+    registration_page = RegistrationPage()
+    registration_page.open()
 
     # заполнение формы
-    registration_form = browser.element('#userForm')
-    registration_form.element('#firstName').should(be.blank).type('John')
-    registration_form.element('#lastName').should(be.blank).type('Week')
-    registration_form.element('#userEmail').should(be.blank).type('john.week@example.com')
-    registration_form.all('#genterWrapper label').element_by(have.text("Male")).click()
-    registration_form.element('#userNumber').should(be.blank).type('9123456789')
-    registration_form.element('#dateOfBirthInput').click()
-    registration_form.element('.react-datepicker__month-dropdown-container--select').click()
-    registration_form.all('.react-datepicker__month-select > option').element_by(have.text("May")).click()
-    registration_form.element('.react-datepicker__year-dropdown-container--select').click()
-    registration_form.all('.react-datepicker__year-select > option').element_by(have.text("2000")).click()
-    registration_form.all('.react-datepicker__day').element_by(have.text("4")).click()
-    registration_form.element('#subjectsContainer').element('.subjects-auto-complete__value-container').click()
-    registration_form.element('.subjects-auto-complete__input input').set_value('Math').press_enter()
-    registration_form.element('#hobbiesWrapper').element('//*[text()=("Sports")]').click()
-    registration_form.element('#hobbiesWrapper').element('//*[text()=("Music")]').click()
-    registration_form.element('#uploadPicture').send_keys(os.path.abspath('data/test01.png'))
-    registration_form.element('#currentAddress').type('ул. Ленина 4')
-    registration_form.element('#state').click().element('#react-select-3-option-2').click()
-    registration_form.all('#state').first().click()
-    registration_form.element('#city').click().element('#react-select-4-option-1').click()
-    registration_form.all('#submit').first().submit()
+
+    registration_page.fill_first_name('John')
+    registration_page.fill_last_name('Week')
+    registration_page.fill_email('john.week@example.com')
+    registration_page.select_gender("Male")
+    registration_page.fill_mobile('9123456789')
+    registration_page.fill_date_of_birth('2000', 'May', '4')
+    registration_page.select_subjects('Math')
+    registration_page.select_hobbies('Sports')
+    registration_page.select_hobbies('Music')
+
+    browser.element('#uploadPicture').send_keys(os.path.abspath('resources/test01.png'))
+    browser.element('#currentAddress').type('ул. Ленина 4')
+
+    registration_page.fill_state('Haryana')
+    registration_page.fill_city('Panipat')
+
+    browser.all('#submit').first().submit()
 
     # Проверка отправленных значений
-    result_form = browser.element('.modal-content')
-    result_form.element("//*[text()=('Student Name')]/../td[2]").should(have.text('John Week'))
-    result_form.element("//*[text()=('Student Email')]/../td[2]").should(have.text('john.week@example.com'))
-    result_form.element("//*[text()=('Gender')]/../td[2]").should(have.text('Male'))
-    result_form.element("//*[text()=('Mobile')]/../td[2]").should(have.text('9123456789'))
-    result_form.element("//*[text()=('Date of Birth')]/../td[2]").should(have.text('04 May,2000'))
-    result_form.element("//*[text()=('Subjects')]/../td[2]").should(have.text('Maths'))
-    result_form.element("//*[text()=('Hobbies')]/../td[2]").should(have.text('Sports, Music'))
-    result_form.element("//*[text()=('Picture')]/../td[2]").should(have.text('test01.png'))
-    result_form.element("//*[text()=('Address')]/../td[2]").should(have.text('ул. Ленина 4'))
-    result_form.element("//*[text()=('State and City')]/../td[2]").should(have.text('Haryana Panipat'))
+    registration_page.should_registered_user_with(
+        'John Week',
+        'john.week@example.com',
+        'Male',
+        '9123456789',
+        '04 May,2000',
+        'Maths',
+        'Sports, Music',
+        'test01.png',
+        'ул. Ленина 4',
+        'Haryana Panipat',
+    )
+
+    # result_form = browser.element('.modal-content')
+    # result_form.element("//*[text()=('Student Name')]/../td[2]").should(have.text('John Week'))
+    # result_form.element("//*[text()=('Student Email')]/../td[2]").should(have.text('john.week@example.com'))
+    # result_form.element("//*[text()=('Gender')]/../td[2]").should(have.text('Male'))
+    # result_form.element("//*[text()=('Mobile')]/../td[2]").should(have.text('9123456789'))
+    # result_form.element("//*[text()=('Date of Birth')]/../td[2]").should(have.text('04 May,2000'))
+    # result_form.element("//*[text()=('Subjects')]/../td[2]").should(have.text('Maths'))
+    # result_form.element("//*[text()=('Hobbies')]/../td[2]").should(have.text('Sports, Music'))
+    # result_form.element("//*[text()=('Picture')]/../td[2]").should(have.text('test01.png'))
+    # result_form.element("//*[text()=('Address')]/../td[2]").should(have.text('ул. Ленина 4'))
+    # result_form.element("//*[text()=('State and City')]/../td[2]").should(have.text('Haryana Panipat'))
